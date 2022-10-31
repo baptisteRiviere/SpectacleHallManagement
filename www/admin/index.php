305849e755a7b43e4131f25f06df41fff5486ec8
baptisteRiviere <riviereb85@gmail.com>
2022-10-31 10:58:11 +0100

    html admin index.html

--- www/admin/index.php
+++ www/admin/index.php
@@ -10,7 +10,22 @@
   </head>
   <body>
     <?php include("../scrollmenu.php"); ?>
-    <p>hello worldn ici c'est asdgdmin</p>
+    
+    <div id="add_spectacle">
+      <h1>add spectacle</h1>
+      <form class="add_spectacle_form" id="add_spectacle_form">
+        <input type="name" placeholder="name" id="name"/>
+        <input type="textarea" placeholder="description" id="description"/>
+        <select name="artist" id="artist">
+          <option value="20">0-20 ans</option>
+          <option value="40" selected>21-40 ans</option>
+          <option value="60">41-60 ans</option>
+          <option value="80">61-80 ans</option>
+        </select>
 
+        <button id="add_spectacle_button">Add spectacle</button>
+        
+      </form>
+    </div>
   </body>
 </html>
