a2b6babcb2dd182823dc5471ae826a771e7909ad
baptisteRiviere <riviereb85@gmail.com>
2022-11-03 13:38:51 +0100

    modif admin/index.js pour ajouter show

--- www/admin/index.js
+++ www/admin/index.js
@@ -1,5 +1,9 @@
+//// ADDING SPECTACLE
+
+var select_artist = document.getElementById('id_artist');
+var add_spectacle_form = document.getElementById('add_spectacle_form');
+
 // requesting the artists list
-var select = document.getElementById('id_artist'); 
 fetch('../accessDB/getArtistList.php')
 .then(r => r.json())
 .then(r => {
@@ -8,25 +12,18 @@ fetch('../accessDB/getArtistList.php')
         const newOption = document.createElement('option');
         newOption.value = artist.id;
         newOption.innerHTML = artist.firstname + " " + artist.lastname;
-        select.appendChild(newOption);
+        select_artist.appendChild(newOption);
     });
 })
 
-
-
-// getting the form to add a spectacle
-var login_form = document.getElementById('add_spectacle_form');
-
 // adding an event when the form is submitted
-login_form.addEventListener('submit', (evnt) => {
+add_spectacle_form.addEventListener('submit', (evnt) => {
     evnt.preventDefault();
 
     // getting information
-    var name = login_form.elements["name"];
-    var description = login_form.elements["description"];
-    var id_artist = login_form.elements["id_artist"];
-
-    console.log(id_artist);
+    var name = add_spectacle_form.elements["name"];
+    var description = add_spectacle_form.elements["description"];
+    var id_artist = add_spectacle_form.elements["id_artist"];
 
     // asking addSpectacle.php
     var data = new FormData();
@@ -41,13 +38,67 @@ login_form.addEventListener('submit', (evnt) => {
     .then(r => {
         let message = document.getElementById('return_add_spectacle');
         if (r.spectacle_added) {
-            message.innerText = "the spectacle has been added";
+            message.innerText = "the spectacle has been added, please refresh to update";
             document.getElementById('name').value = "";
             document.getElementById('description').value = "";
         } else {
             message.innerText = r.error;
         }
-        
     })
 });
 
+
+
+
+//// ADDING SHOW
+
+var select_spectacle = document.getElementById('id_spectacle'); 
+var add_show_form = document.getElementById('add_show_form');
+
+// requesting the spectacles list
+fetch('../accessDB/getSpectacleList.php')
+.then(r => r.json())
+.then(r => {
+    r.forEach(spectacle => {
+        // create option using DOM
+        const newOption = document.createElement('option');
+        newOption.value = spectacle.id;
+        newOption.innerHTML = spectacle.name;
+        select_spectacle.appendChild(newOption);
+    });
+})
+
+
+// adding an event when the form is submitted
+add_show_form.addEventListener('submit', (evnt) => {
+    evnt.preventDefault();
+
+    // getting information
+    var date = add_show_form.elements["date"];
+    var time = add_show_form.elements["time"];
+    var halfless = add_show_form.elements["halfless"];
+    var id_spectacle = add_show_form.elements["id_spectacle"];
+
+    // asking addShow.php
+    var data = new FormData();
+    data.append('date',date.value);
+    data.append('time',time.value);
+    data.append('halfless',halfless.value);
+    data.append('id_spectacle',id_spectacle.value);
+    fetch('../accessDB/addShow.php', {
+        method: 'post',
+        body: data
+    })
+    .then(r => r.json())
+    .then(r => {
+        console.log(r);
+        /*
+        let message = document.getElementById('return_add_show');
+        if (r.spectacle_added) {
+            message.innerText = "the show has been added, please refresh to update";
+        } else {
+            message.innerText = r.error;
+        }
+        */
+    })
+});
--- www/admin/index.php
+++ www/admin/index.php
@@ -14,10 +14,10 @@
     <div id="add_spectacle">
       <h1>add spectacle</h1>
       <form class="add_spectacle_form" id="add_spectacle_form">
-        <input type="name" placeholder="name" id="name"/>
-        <input type="textarea" placeholder="description" id="description"/>
-        <select name="id_artist" id="id_artist"></select>
-        <button id="add_spectacle_button">Add spectacle</button>
+        <label>name<input type="name" placeholder="name" id="name"/></label>
+        <label>description<input type="textarea" placeholder="description" id="description"/></label>
+        <label>artist<select name="id_artist" id="id_artist"></select></label>
+        <label><button id="add_spectacle_button">Add spectacle</button>
         <p id="return_add_spectacle"></p>
       </form>
     </div>
@@ -25,10 +25,10 @@
     <div id="add_show">
       <h1>add show</h1>
       <form class="add_show_form" id="add_show_form">
-        <input type="date" placeholder="date" id="date"/>
-        <input type="time" placeholder="time" id="time">
-        <input type="checkbox" placeholder="halfless" id="halfless">
-        <select name="id_spectacle" id="id_spectacle"></select>
+        <label>date<input type="date" placeholder="date" id="date"/></label>
+        <label>time<input type="time" placeholder="time" id="time"></label>
+        <label>half less<input type="checkbox" placeholder="halfless" id="halfless"></label>
+        <label>spectacle<select name="id_spectacle" id="id_spectacle"></select></label>
         <button id="add_show_button">Add show</button>
         <p id="return_add_show"></p>
       </form>
