a56c9d43681eabfe98a0b3288d1a0c5ec13f6667
baptisteRiviere <riviereb85@gmail.com>
2022-11-05 11:56:49 +0100

    ajout information place getTicketList.php

--- www/accessDB/getTicketList.php
+++ www/accessDB/getTicketList.php
@@ -2,11 +2,12 @@
 
     include "connect.php";
 
-    $request = "SELECT * FROM ticket";
+    $request = "SELECT ti.id, ti.id_spectator, pl.location, pl.category, pl.price FROM ticket AS ti
+                JOIN place as pl ON ti.id_place = pl.id";
 
     if (isset($_POST['showdate_id'])) {
         $showdate_id = $_POST['showdate_id'];
-        $request = $request . " WHERE id_showDate = $showdate_id";
+        $request = $request . " WHERE ti.id_showDate = $showdate_id";
     } 
         
     // asking database
