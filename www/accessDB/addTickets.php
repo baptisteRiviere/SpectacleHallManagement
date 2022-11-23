48b977bed0db72a0d8ae76e87192781ef7045d43
baptisteRiviere <riviereb85@gmail.com>
2022-11-23 21:02:59 +0100

    fix ajout showdate par admin

--- www/accessDB/addTickets.php
+++ www/accessDB/addTickets.php
@@ -17,40 +17,32 @@
     }
 
     // getting avery places id
-    $places_request = "SELECT id FROM place";
+    $places_request = "SELECT id,location_y FROM place";
     $places_response = [];
     if ($result = mysqli_query($link,$places_request)) {
       while ($line = mysqli_fetch_assoc($result)) {
-        $places_response[] = $line["id"];
+        $places_response[] = $line;
       }
     }
 
     // for each place a ticket is created
-    $add_ticket_request = "INSERT INTO ticket (id_showDate,id_place) VALUES";     // request to write new spectacle in database
-    foreach ($places_response as $id_place) {
-      $place_covid = ((int) $id_place % 2 == 1);
-      echo "places covid : " . $place_covid;
-      echo "\n";
-      echo "halfless : " . $halfless;
-      echo "\n";
-      if ($halfless && $place_covid) { // if we want to have half places
-        echo "boucle ok";
+    $add_ticket_request = "INSERT INTO ticket (id_showDate,id_place,id_spectator) VALUES";     // request to write new spectacle in database
+    foreach ($places_response as $place) {
+      $id_place = $place["id"];
+      if (filter_var($halfless, FILTER_VALIDATE_BOOLEAN) and ((int) $place["location_y"] % 2 === 0)) {
+        $add_ticket_request .= " ($id_showDate,$id_place,2),";
       } else {
-        echo "boucle non";
-        $add_ticket_request .= " ($id_showDate,$id_place),";
+        $add_ticket_request .= " ($id_showDate,$id_place,NULL),";
       }
-      echo "\n";
     }
     $add_ticket_request = substr_replace($add_ticket_request,";",-1);
 
-    /*
     // writing request
     if ($result = mysqli_query($link,$add_ticket_request)) {
       echo json_encode(array("tickets_added" => true));
     } else {
       echo json_encode(array("error" => "Error : the tickets hasn't been added"));
     }
-    */
 
   } else {
     echo json_encode(array('error' => "Error : data hasn't been well delivered"));
